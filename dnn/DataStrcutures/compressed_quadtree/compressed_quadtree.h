108a1ad88f88d13e21254dbe8df5054b9b34b0f7
Soeun Lee <soeunlee@postech.ac.kr>
2019-11-06 19:36:50 +0900

    :arrow_up: init code to origin

--- dnn/DataStrcutures/compressed_quadtree/compressed_quadtree.h
+++ dnn/DataStrcutures/compressed_quadtree/compressed_quadtree.h
@@ -1,7 +1,5 @@
-#include <
-
 /*
-Copyright (c) 2010 Daniel Minor 
+Copyright (c) 2010 Daniel Minor
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,12 +24,12 @@ THE SOFTWARE.
 #define COMPRESSED_QUADTREE_H_
 
 /*
-    Compressed Quadtree implementation supporting approximate nearest neighbour
-    queries, based upon the description in:  
-    
-    Eppstein, D., Goodrich, M. T., Sun, J. Z. (2008) The Skip Quadtree:
-    A Simple Dynamic Data Structure for Multidimensional Data,
-    Int. Journal on Computational Geometry and Applications, 18(1/2), pp. 131 - 160
+	Compressed Quadtree implementation supporting approximate nearest neighbour
+	queries, based upon the description in:
+
+	Eppstein, D., Goodrich, M. T., Sun, J. Z. (2008) The Skip Quadtree:
+	A Simple Dynamic Data Structure for Multidimensional Data,
+	Int. Journal on Computational Geometry and Applications, 18(1/2), pp. 131 - 160
 */
 
 #include <algorithm>
@@ -40,257 +38,297 @@ THE SOFTWARE.
 #include <vector>
 #include <iostream>
 #include <cstring>
-#include <queue>
-#include <set>
-#include <cmath>
-#include <map>
 
 template<class Point> class CompressedQuadtree {
 
-    public:
+public:
 
-        //Nodes of the quadtree
-        struct Node { 
-			private:
-			static size_t ident;
-			public :
+	//Nodes of the quadtree
+	struct Node {
+		Node** nodes;       //children
+		Point mid;          //midpoint
+		double radius;      //half side length
+		Point* pt;          //point, if data stored
+	};
 
-			Node * parrent; 	//parrent
-			std::vector<Node *> nodes;       //children
-            Point mid;          //midpoint
-            double radius;      //half side length
-			Point *pt;
-			size_t id;
-			int D;
-			std::set<size_t> Cand;
-			Node(){
-				id=ident;
-				ident++;
-				D=-1;
-			}
+	//Keep track of point and priority for nearest neighbour search
+	struct NodeDistance {
 
-		};	
-	
-		std::map<size_t,Node *> map;
-		std::map<size_t,Node *> leaf;
-		
-		Node* Getleaf(size_t id){
-			return leaf[id];
-		}
+		Node* node;
+		double distance;
 
-		Node* GetNode(size_t id){
-			return map[id];
-		}
+		NodeDistance(Node* node, double distance) : node(node), distance(distance) {};
 
-		class NodePointerCmp
+		//min-heap
+		bool operator<(const NodeDistance& other) const
 		{
-  			public:
-    		NodePointerCmp(){}
-		 	 bool operator() (Node* lhs,Node* rhs)
-		    {
-				return lhs->radius<rhs->radius;
-			}
-		};
+			return distance > other.distance;
+		}
+	};
 
-		double dist(Node *a,Node* b){
-			double ra=a->radius;
-			double rb=b->radius;
-			bool left = a->mid[0]+ra < b->mid[0]-rb;
-			bool right = a->mid[0]-ra > b->mid[0]+rb;
-			bool top = a->mid[1]-ra > b->mid[1]+rb;
-			bool bottom = a->mid[1]+ra < b->mid[1]-rb;
-
-			if(left && top) return sqrt((a->mid[0]+ra-b->mid[0]+rb)*(a->mid[0]+ra-b->mid[0]+rb)+(a->mid[1]-b->mid[1]-ra-rb)*(a->mid[1]-b->mid[1]-ra-rb));
-			if(left && bottom) return sqrt((a->mid[0]+ra-b->mid[0]+rb)*(a->mid[0]+ra-b->mid[0]+rb)+(a->mid[1]-b->mid[1]+ra+rb)*(a->mid[1]-b->mid[1]+ra+rb));
-			if(right && top) return sqrt((a->mid[0]-ra-b->mid[0]-rb)*(a->mid[0]-ra-b->mid[0]-rb)+(a->mid[1]-b->mid[1]-ra-rb)*(a->mid[1]-b->mid[1]-ra-rb));
-			if(right && bottom) return sqrt((a->mid[0]-ra-b->mid[0]-rb)*(a->mid[0]-ra-b->mid[0]-rb)+(a->mid[1]-b->mid[1]+ra+rb)*(a->mid[1]-b->mid[1]+ra+rb));
-
-			if(top) return abs(a->mid[1]-b->mid[1]-ra-rb);
-			if(bottom) return abs(a->mid[1]-b->mid[1]+ra+rb);
-			if(left) return abs(a->mid[0]+ra-b->mid[0]+rb);
-			if(right) return abs(a->mid[0]-b->mid[0]-ra-rb);
-			return 0.0;
+	CompressedQuadtree(size_t dim, Point* pts, size_t n) : dim(dim), nnodes(1 << dim), locate_eps(0.001)
+	{
+		//calculate bounds
+		Point bounds[2];
+		for (size_t d = 0; d < dim; ++d) {
+			bounds[0][d] = std::numeric_limits<double>::max();
+			bounds[1][d] = -std::numeric_limits<double>::max();
 		}
 
-		bool Candcheck(Node *a, Node* b,double delta){
-				double ra=a->radius;
-				double rb=b->radius;
-				Node* tmp;
-				if(ra>rb) return false;
-				if(rb/ra <= 4*delta){
-					if(dist(a,b)<=4*sqrt(2)*(ra+rb))
-						return true;
-					else return false;
-				}
-				else if(b->nodes.size()<2){
-					if(b->nodes.size()==1){
-						tmp=b->nodes[0];	
-						if(tmp->radius>4*delta*ra) return false;	
-					}
-					if(dist(a,b)<=4*sqrt(2)*ra*(1+4*delta)) return true;
-				}
-				return false;
-		}
-
-		void CalCand(double delta){
-			int count=0;
-			std::priority_queue<Node*,std::vector<Node*>,NodePointerCmp> bfsQ;
-			bfsQ.push(root);
-			map[root->id]=root;
-			while(!bfsQ.empty()){
-				Node* tmp= bfsQ.top();
-				bfsQ.pop();	
-          		if (tmp!=root) {
-					Node * pa=tmp->parrent;
-					std::set<size_t>::iterator it;
-					for(it=pa->Cand.begin();it!=pa->Cand.end();it++){
-						if(Candcheck(tmp,map[*it],delta)){
-							tmp->Cand.insert(*it);
-							map[*it]->Cand.insert(tmp->id);
-						}
-					}
-				}
-		
-				for (size_t i=0; i<tmp->nodes.size(); ++i) {
-					bfsQ.push(tmp->nodes[i]);
-					map[tmp->nodes[i]->id]=tmp->nodes[i];
-					for (size_t j=i+1;j<tmp->nodes.size();++j){
-						tmp->nodes[i]->Cand.insert(tmp->nodes[j]->id);
-						tmp->nodes[j]->Cand.insert(tmp->nodes[i]->id);
-					}
-				}
+		for (size_t d = 0; d < dim; ++d) {
+			for (size_t i = 0; i < n; ++i) {
+				if (pts[i][d] < bounds[0][d]) bounds[0][d] = pts[i][d];
+				if (pts[i][d] > bounds[1][d]) bounds[1][d] = pts[i][d];
 			}
 		}
 
-        CompressedQuadtree(size_t dim, Point *pts, size_t n) : dim(dim), nnodes(1 << dim)//, locate_eps(0.001)
-        { 
-            //calculate bounds
-            Point bounds[2];
-            for (size_t d = 0; d < dim; ++d) {
-                bounds[0][d] = std::numeric_limits<double>::max(); 
-                bounds[1][d] = -std::numeric_limits<double>::max(); 
-            }
+		//calculate mid point and half side length
+		Point mid, side;
+		double radius = 0;
+		for (size_t d = 0; d < dim; ++d) {
+			mid[d] = (bounds[0][d] + bounds[1][d]) / 2;
+			double side = (bounds[1][d] - bounds[0][d]) / 2;
+			if (side > radius) radius = side;
+		}
 
-            for (size_t d = 0; d < dim; ++d) {
-                for (size_t i = 0; i < n; ++i) {
-                    if (pts[i][d] < bounds[0][d]) bounds[0][d] = pts[i][d];
-                    if (pts[i][d] > bounds[1][d]) bounds[1][d] = pts[i][d];
-                }
-            }
- 
-            //calculate mid point and half side length
-            Point mid, side; 
-            double radius = 0;
-            for (size_t d = 0; d < dim; ++d) {
-                mid[d] = (bounds[0][d]+bounds[1][d]) / 2;
-                double side = (bounds[1][d]-bounds[0][d]) / 2;
-                if (side > radius) radius = side;
-            } 
+		//set up points vector 
+		std::vector<Point*> pts_vector;
+		for (size_t i = 0; i < n; ++i) {
+			pts_vector.push_back(&pts[i]);
+		}
 
-            //set up points vector 
-            std::vector<Point *> pts_vector;
-            for (size_t i = 0; i < n; ++i) {
-                pts_vector.push_back(&pts[i]);
-            }
+		root = worker(mid, radius, pts_vector);
+	}
 
-            root = worker(mid, radius, pts_vector,NULL);
-        }
+	virtual ~CompressedQuadtree()
+	{
+		if (root) delete_worker(root);
+	}
 
-        virtual ~CompressedQuadtree()
-        {
-           if (root) delete_worker(root);  
-        }
+	std::list<std::pair<Point*, double> > knn(size_t k, const Point& pt, double eps)
+	{
+		//setup query result vector
+		std::list<std::pair<Point*, double> > qr;
 
-        Node *root;
-		
-    private:
-        size_t dim; 
-        size_t nnodes;
+		//initialize priority queue for search
+		std::vector<NodeDistance > pq;
+		pq.push_back(NodeDistance(root, 0.0));
 
+		while (!pq.empty()) {
 
-        Node *worker(const Point &mid, double radius, std::vector<Point *> &pts,Node* parrent)
-        {
-            Node *node = new Node; 
-			node->parrent=parrent;
-			
-			for (size_t d = 0; d < dim; ++d) {
-                node->mid[d] = mid[d];
-            }
-            node->radius = radius; 
+			std::pop_heap(pq.begin(), pq.end());
+			Node* node = pq.back().node;
+			double node_dist = pq.back().distance;
+			pq.pop_back();
 
-            if (pts.size() == 1) {
-                node->pt = pts[0];
-				//printf("%d ",node->pt->id);
-				leaf[node->pt->id]=node;
-            } else { 
-                node->pt = 0;
+			if (node->nodes == 0) {
+				//calculate distance from query point to this point
+				double dist = 0.0;
+				for (size_t d = 0; d < dim; ++d) {
+					dist += ((*node->pt)[d] - pt[d]) * ((*node->pt)[d] - pt[d]);
+				}
 
-                //divide points between the nodes 
-                std::vector<Point *> *node_pts = new std::vector<Point *>[nnodes];
-                for (typename std::vector<Point *>::iterator itor = pts.begin(); itor != pts.end(); ++itor) {
+				//insert point in result
+				typename std::list<std::pair<Point*, double> >::iterator itor = qr.begin();
+				while (itor != qr.end() && itor->second < dist) {
+					++itor;
+				}
 
-                    //determine node index based upon which which side of midpoint for each dimension
-                    size_t n = 0;
-                    for (size_t d = 0; d < dim; ++d) {
-                        if ((*(*itor))[d] > mid[d]) n += 1 << d; 
-                    } 
+				qr.insert(itor, std::make_pair<Point*, double>(node->pt, dist));
 
-                    node_pts[n].push_back(*itor);
-                } 
+				if (qr.size() > k) qr.pop_back();
 
-                //create new nodes recursively
-                size_t n_nodes = 0;
-                for (size_t n = 0; n < nnodes; ++n) {
+			}
+			else {
 
-                    if (node_pts[n].size()) {
+				//find k-th distance
+				double kth_dist = qr.size() < k ? std::numeric_limits<double>::max() : qr.back().second;
 
-                        Point nbounds[2];
-                        Point new_mid;
-                        double new_radius = radius / 2.0;
-                        for (size_t d = 0; d < dim; ++d) { 
-                            if (n & (1 << d)) {
-                                new_mid[d] = mid[d] + new_radius;
-                            } else { 
-                                new_mid[d] = mid[d] - new_radius;
-                            }
-                        }
-						Node * tmp = worker(new_mid, new_radius, node_pts[n],node);
+				//stop searching, all further nodes farther away than k-th value
+				if (kth_dist <= (1.0 + eps) * node_dist) {
+					break;
+				}
 
-                        node->nodes.push_back(tmp);
+				for (size_t n = 0; n < nnodes; ++n) {
+					//calculate distance to each of the non-zero children
+					//if less than k-th distance, then visit 
+					if (node->nodes[n]) {
+
+						double min_dist = min_pt_dist_to_node(pt, node->nodes[n]);
+
+						//if closer than k-th distance, search
+						if (min_dist < kth_dist) {
+							pq.push_back(NodeDistance(node->nodes[n], min_dist));
+							std::push_heap(pq.begin(), pq.end());
 						}
-                }
+					}
+				}
+			}
+		}
 
-                delete[] node_pts;
-                
-				//compress if less than 2 interesting nodes
-                if (node->nodes.size() == 1) {
-					if (node->nodes[0]) {
-						if(node->nodes[0]->nodes.size()==1){
-                            Node *re = node->nodes[0];
-                            Node *ad=node->nodes[0]->nodes[0];
-							node->nodes[0]=ad;
-							ad->parrent=node;
-                            delete re;
+		return qr;
+	}
+
+	Node* locate(const Point& pt)
+	{
+
+		Node* node = 0;
+
+		//search for node containing the query point 
+		if (in_node(root, pt)) {
+			node = root;
+
+			while (node) {
+				if (node->nodes) {
+					size_t n = 0;
+					for (size_t d = 0; d < dim; ++d) {
+						if (pt[d] > node->mid[d]) n += 1 << d;
+					}
+
+					if (node->nodes[n] && in_node(node->nodes[n], pt)) node = node->nodes[n];
+					else break;
+
+				}
+				else {
+					break;
+				}
+			}
+		}
+
+		return node;
+	}
+
+	Node* root;
+	double locate_eps;
+
+private:
+
+	size_t dim;
+	size_t nnodes;
+
+	Node* worker(const Point& mid, double radius, std::vector<Point*>& pts)
+	{
+		Node* node = new Node;
+		for (size_t d = 0; d < dim; ++d) {
+			node->mid[d] = mid[d];
+		}
+		node->radius = radius;
+
+		if (pts.size() == 1) {
+			node->nodes = 0;
+			node->pt = pts[0];
+		}
+		else {
+			node->nodes = new Node * [nnodes];
+			node->pt = 0;
+
+			//divide points between the nodes 
+			std::vector<Point*>* node_pts = new std::vector<Point*>[nnodes];
+			for (typename std::vector<Point*>::iterator itor = pts.begin(); itor != pts.end(); ++itor) {
+
+				//determine node index based upon which which side of midpoint for each dimension
+				size_t n = 0;
+				for (size_t d = 0; d < dim; ++d) {
+					if ((*(*itor))[d] > mid[d]) n += 1 << d;
+				}
+
+				node_pts[n].push_back(*itor);
+			}
+
+			//create new nodes recursively
+			size_t ninteresting = 0;
+			for (size_t n = 0; n < nnodes; ++n) {
+
+				if (node_pts[n].size()) {
+
+					Point nbounds[2];
+					Point new_mid;
+					double new_radius = radius / 2.0;
+					for (size_t d = 0; d < dim; ++d) {
+						if (n & (1 << d)) {
+							new_mid[d] = mid[d] + new_radius;
 						}
-                    }
-                }
-            } 
+						else {
+							new_mid[d] = mid[d] - new_radius;
+						}
+					}
 
-            return node;
-        } 
+					++ninteresting;
+					node->nodes[n] = worker(new_mid, new_radius, node_pts[n]);
+				}
+				else {
+					node->nodes[n] = 0;
+				}
+			}
 
-        void delete_worker(Node *node)
-        {
-        	for (size_t n = 0; n < node->nodes.size(); ++n) { 
-                	delete_worker(node->nodes[n]); 
-            }
-            delete node; 
-        }    
+			delete[] node_pts;
+
+			//compress if less than 2 interesting nodes
+			if (ninteresting < 2) {
+				for (size_t n = 0; n < nnodes; ++n) {
+					if (node->nodes[n]) {
+						Node* temp = node;
+						node = node->nodes[n];
+						delete temp;
+						break;
+					}
+				}
+			}
+		}
+
+		return node;
+	}
+
+	double min_pt_dist_to_node(const Point& pt, Node* node)
+	{
+		bool inside = true;
+		double min_dist = std::numeric_limits<double>::max();
+		for (size_t d = 0; d < dim; ++d) {
+
+			double dist;
+			if (pt[d] < node->mid[d] - node->radius) {
+				dist = node->mid[d] - node->radius - pt[d];
+				inside = false;
+			}
+			else if (pt[d] > node->mid[d] + node->radius) {
+				dist = pt[d] - (node->mid[d] + node->radius);
+				inside = false;
+			}
+
+			if (dist < min_dist) min_dist = dist;
+		}
+
+		if (inside) return 0.0;
+		else return min_dist * min_dist;
+	}
+
+	bool in_node(const Node* node, const Point& pt)
+	{
+		bool in = true;
+
+		for (size_t d = 0; d < dim; ++d) {
+			if (root->mid[d] - root->radius - pt[d] > locate_eps || pt[d] - root->mid[d] - root->radius > locate_eps) {
+				in = false;
+				break;
+			}
+		}
+
+		return in;
+	}
+
+	void delete_worker(Node* node)
+	{
+		if (node->nodes) {
+			for (size_t n = 0; n < nnodes; ++n) {
+				if (node->nodes[n]) delete_worker(node->nodes[n]);
+			}
+
+			delete node->nodes;
+		}
+
+		delete node;
+	}
 
 };
 
-template<class Point>
-size_t CompressedQuadtree<Point>::Node::ident = 0;
-
 #endif
-
