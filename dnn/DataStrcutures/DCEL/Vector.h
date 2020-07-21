f1386388fec5ec012066422b9bd2a2c928ed6fa4
정재훈 <sk7755@postech.ac.kr>
2020-07-21 16:27:23 +0900

    :art: make vector class

--- dnn/DataStrcutures/DCEL/Vector.h
+++ dnn/DataStrcutures/DCEL/Vector.h
@@ -1,23 +1,20 @@
 #pragma once
 #include "Point.h"
-#include "Edge.h"
+//#include "Edge.h"
 
 
-class Vector {
-protected:
-	Point origin, dest;
-
+class Vector : public Point{
 public:
-
 	Vector();
-	Vector(Point *, Point*);
-	Vector(Edge *);
+	Vector(Point );
+	Vector(Point , Point);
+	//Vector(Edge );	//??
 	~Vector();
-	bool operator==(Vector *);
-	Vector operator- (Vector *);
-	double innerProdct(Vector *);
-	double outerProdct(Vector *);
-	Point getOrigin(void);
-	Point getDest(void);
+	bool operator==(Vector );
+	Vector operator- (Vector );
+	double innerProdct(Vector );
+	double outerProdct(Vector );
+	//Point getOrigin(void);
+	//Point getDest(void);
 	double norm();
 };
