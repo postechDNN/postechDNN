40a8cd386ff15ad4804a3cfe92d5757cf52f2262
Mincheol Kim <54087389+RekhAcetreet@users.noreply.github.com>
2020-09-24 21:44:49 +0900

    :memo: Update footnote

--- dnn/DataStructures/CarrierGraph/Rect.h
+++ dnn/DataStructures/CarrierGraph/Rect.h
@@ -4,21 +4,21 @@ using namespace std;
 
 class Rect {
 protected:
-	double l,r,u,d;
-	Point *ld, *lu, *rd, *ru;
-	int id;
+	double l,r,u,d; // four double values indicating coordinates of each sides of rectangle
+	Point *ld, *lu, *rd, *ru; // four corners
+	int id; // id of rectangle
 public:
-	Rect(double, double, double, double);
-	~Rect();
-	double getl(void);
-	double getr(void);
-	double getu(void);
-	double getd(void);
-	const bool operator==(Rect&);
-	Point* getld(void);
-	Point* getlu(void);
-	Point* getrd(void);
-	Point* getru(void);
-	int getid();
-	void setid(int);
+	Rect(double, double, double, double); // constructor with four sides, {left, bottom, right, top}
+	~Rect(); // destructor
+	double getl(void); // get left value of rectangle
+	double getr(void); // get right value of rectangle
+	double getu(void); // get top value of rectangle
+	double getd(void); // get bottom value of rectangle
+	const bool operator==(Rect&); // return true if two rectangles are same
+	Point* getld(void); // get the left-bottom corner of rectangle
+	Point* getlu(void); // get the left-top corner of rectangle
+	Point* getrd(void); // get the right-bottom corner of rectangle
+	Point* getru(void); // get the right-top corner of rectangle
+	int getid(); // get id of rectangle
+	void setid(int); //set id of rectangle
 };
