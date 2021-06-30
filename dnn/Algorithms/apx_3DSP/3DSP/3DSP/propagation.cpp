f4d0debd01a032e60da9257f54e70b1ad33dd6d7
tkeom0114 <tkeom0114@postech.ac.kr>
2021-06-30 23:01:49 +0900

    :arrow_up:Implement propagation algorithm in 3DSP

--- dnn/Algorithms/apx_3DSP/3DSP/3DSP/propagation.cpp
+++ dnn/Algorithms/apx_3DSP/3DSP/3DSP/propagation.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include "propagation.h"
 
 void Segment::SetAdjDiagram()
@@ -9,10 +10,10 @@ void Segment::SetAdjDiagram()
 	{
 		for (size_t j = 0; j < Adjs.size(); j++)
 		{
-			Segment l1 = *Adjs[j].first;
+			Segment& l1 = *Adjs[j].first;
 			if (Adjs[j].second == -1)
 			{
-				AdjDiagram[i][j] = pair<double, double>(0, 1);
+				AdjDiagram[i][j] = { 0, 1 };
 				continue;
 			}
 			Tri f = Tris[Adjs[j].second];
@@ -20,11 +21,11 @@ void Segment::SetAdjDiagram()
 			v.push_back(f.p1);
 			v.push_back(f.p2);
 			v.push_back(f.p3);
-			auto res = pair<double, double>(0., 1.);
+			pair<double, double> res(0., 1.);
 			for (size_t k = 0; k < 3; k++)
 			{
 				auto temp = Interval(i, v[k], v[(k + 1) % 3], v[(k + 2) % 3], f, l1);
-				res = pair<double, double>(max(res.first, temp.first), min(res.second, temp.second));
+				res = { max(res.first, temp.first), min(res.second, temp.second) };
 			}
 			AdjDiagram[i][j] = res;
 		}
@@ -38,25 +39,25 @@ pair<double, double> Segment::Interval(int i,MyVec& v1, MyVec& v2, MyVec& v3, Tr
 	if (abs(InnerProd(n, l1.b - l1.a)) < EPS)
 	{
 		if (InnerProd(n, v3 - v) * InnerProd(n, l1.a - v) > 0)
-			return pair<double, double>(0., 1.);
+			return { 0., 1. };
 		else
-			return pair<double, double>(1., 0.);
+			return { 1., 0. };
 	}
 	double t = InnerProd(v - l1.a, n) / InnerProd(l1.b - l1.a, n);
 	if (InnerProd(l1.b - l1.a, n) * InnerProd(v3 - v, n) > 0)
-		return pair<double, double>(t, 1.);
+		return { t, 1. };
 	else
-		return pair<double, double>(0., t);
+		return { 0., t };
 }
 
 void Segment::SetNear()
 {
-	for (size_t i = 0; i < X.size(); i++)
+	for (size_t i = 0; i < Adjs.size(); i++)
 	{
-		for (size_t j = 0; j < Adjs.size(); j++)
+		for (size_t j = 0; j < X.size(); j++)
 		{
-			Segment l1 = *Adjs[j].first;
-			MyVec v = (b - a) * X[i] + a;
+			Segment& l1 = *Adjs[i].first;
+			MyVec v = (b - a) * X[j] + a;
 			double t = InnerProd(l1.a - l1.b, l1.a - v) / InnerProd(l1.b - l1.a, l1.b - l1.a);
 			Near[i][j] = min(max(t, 0.), 1.);
 		}
@@ -66,15 +67,15 @@ void Segment::SetNear()
 //dist[i]>=dist[i1] 이고 Segment l위의 site가 X[i] 와 X[i1]에 있을 때, X[i]가 차지하는 l1위의 구간을 구함
 pair<double, double> Segment::Vinterval(int i, int i1, int lindex)
 {
-	Segment l1 = *Adjs[lindex].first;
+	Segment& l1 = *Adjs[lindex].first;
 	double c = dist[i] - dist[i1];
 	if (abs(c) < EPS)
 	{
 		double t = InnerProd(b - a, a + ((b - a) * (X[i] + X[i1])) / 2 - l1.a) / InnerProd(b - a, l1.b - l1.a);
 		if (InnerProd(b - a, l1.b - l1.a) > 0) 
-			return pair<double, double>(t,1);
+			return { t,1. };
 		else
-			return pair<double, double>(0,t);
+			return { 0.,t };
 	}
 	double Bbar = (X[i] - X[i1]) * InnerProd(b - a, (l1.a - a) * 2 - (b - a) * (X[i] + X[i1])) - c * c;
 	double A = 4 * InnerProd(l1.b - l1.a, l1.b - l1.a) * (c * c - (X[i] - X[i1]) * (X[i] - X[i1]) * InnerProd(b - a, b - a));
@@ -83,56 +84,71 @@ pair<double, double> Segment::Vinterval(int i, int i1, int lindex)
 
 	double D = B * B - 4 * A * C;
 	if (D < EPS)
-		return pair<double, double>(1., 0.);
+		return { 1., 0. };
 	if (abs(A) < EPS)
 	{
 		double t = -C / B;
 		if (!IsVvertex(i, i1, l1, t))
-			return pair<double, double>(0., 1.);
+			return { 0., 1. };
 		if (IsVvertex(i, i1, l1, t + 1))
-			return pair<double, double>(t, 1.);
-		return pair<double, double>(0., t);
+			return { t, 1. };
+		return { 0., t };
 	}
 	double t1 = (-B - sqrt(D)) / (2 * A);
 	double t2 = (-B + sqrt(D)) / (2 * A);
 	bool val1 = IsVvertex(i, i1, l1, t1);
 	bool val2 = IsVvertex(i, i1, l1, t2);
 	if (val1 && val2)
-		return pair<double, double>(max(0., t1), min(1., t2));
+		return { max(0., t1), min(1., t2) };
 	else if (!val1 && val2)
-		return pair<double, double>(t2, 1.);
+		return { t2, 1. };
 	else if (val1 && !val2)
-		return pair<double, double>(0., t1);
+		return { 0., t1 };
 	else
-		return pair<double, double>(1., 0.);
+		return { 1., 0. };
 }
 
 
 bool Segment::IsActive(int i)
 {
-	return static_cast<bool>(S.count(i));
+	return static_cast<bool>(S.count(X[i]));
 }
 
-void Segment::UpdateV(int i, int lindex)
+void Segment::Update(int i, double val, priority_queue<Repr, vector<Repr>>& Reprs)
 {
-	Segment l1 = *Adjs[lindex].first;
+	dist[i] = val;
+	for (size_t lindex = 0; lindex < Adjs.size(); lindex++)
+		UpdateSeg(i, lindex, Reprs);
+	S[X[i]] = i;
+	Sbar.erase(X[i]);
+}
+
+void Segment::UpdateSeg(int i, int lindex, priority_queue<Repr, vector<Repr>>& Reprs)
+{
+	Segment& l1 = *Adjs[lindex].first;
 	set<pair<double, int>>& addV = AddVoronoi[lindex];
+	if (addV.empty())
+	{
+		addV.insert({ 0.,-1 });
+		addV.insert({ 1.,i });
+		return;
+	}
 	// k번째 원소 O(log n)에 계산가능한 tree 구현해서 수정해야 함
 	vector<pair<double, int>> vList;
 	vList.assign(addV.begin(), addV.end());
 
 	int start = 0;
 	int end = addV.size() - 1;
-	int i2;
+	int i1;
 	double x1, x2;
 	while (true)
 	{
 		if (end - start < 2)
 		{
-			pair<double, double> intv(0, 1);
-			for (int j = max(0,start-1); j <= min(end, static_cast<int>(addV.size() - 1)); j++)
+			pair<double, double> intv(0., 1.);
+			for (int j = max(0,start); j <= min(end+1, static_cast<int>(addV.size() - 1)); j++)
 			{
-				pair<double, double> temp = Vinterval(i, vList[j].second, lindex);
+				auto temp = Vinterval(i, vList[j].second, lindex);
 				intv.first = max(intv.first, temp.first);
 				intv.second = min(intv.second, temp.second);
 			}
@@ -151,15 +167,15 @@ void Segment::UpdateV(int i, int lindex)
 			end = mid;
 			continue;
 		}
-		pair<double, double> intv(0, 1);
-		for (int j = mid-1; j < mid+1; j++)
+		pair<double, double> intv(0., 1.);
+		for (int j = mid; j < mid+2; j++)
 		{
-			pair<double, double> temp = Vinterval(i, vList[j].second, lindex);
+			auto temp = Vinterval(i, vList[j].second, lindex);
 			intv.first = max(intv.first, temp.first);
 			intv.second = min(intv.second, temp.second);
 		}
 		if (intv.first > intv.second)
-			return ;
+			return;
 		if (vList[mid].first > intv.first)
 			end = mid;
 		else
@@ -170,16 +186,15 @@ void Segment::UpdateV(int i, int lindex)
 		if (end - start < 2)
 		{
 			pair<double, double> intv(0, 1);
-			for (int j = max(0, start - 1); j <= min(end, static_cast<int>(addV.size()-1)); j++)
+			for (int j = max(0, start); j <= min(end+1, static_cast<int>(addV.size()-1)); j++)
 			{
-				pair<double, double> temp = Vinterval(i, vList[j].second, lindex);
+				auto temp = Vinterval(i, vList[j].second, lindex);
 				intv.first = max(intv.first, temp.first);
 				intv.second = min(intv.second, temp.second);
 			}
 			if (intv.first > intv.second)
 				return;
 			x2 = intv.second;
-			i2 = vList[start].second;
 			break;
 		}
 		int mid = (start + end) / 2;
@@ -193,9 +208,9 @@ void Segment::UpdateV(int i, int lindex)
 			continue;
 		}
 		pair<double, double> intv(0., 1.);
-		for (int j = mid - 1; j < mid + 1; j++)
+		for (int j = mid; j < mid + 2; j++)
 		{
-			pair<double, double> temp = Vinterval(i, vList[j].second, lindex);
+			auto temp = Vinterval(i, vList[j].second, lindex);
 			intv.first = max(intv.first, temp.first);
 			intv.second = min(intv.second, temp.second);
 		}
@@ -206,13 +221,99 @@ void Segment::UpdateV(int i, int lindex)
 		else
 			start = mid;
 	}
-	
-	auto it = addV.lower_bound(pair<double, int>(x1, 0));
-	while (it != addV.lower_bound(pair<double, int>(x2, sizeX())))
-		it = addV.erase(it);
-	addV.emplace(x1, i);
-	addV.emplace(x2, i2);
-	return ;
+	if (x1 >= x2)
+		return;
+	SetReprInv(i, lindex, Reprs);
+	auto it = addV.lower_bound({ x1, -1 });
+	i1 = it->second;
+	if (it != addV.begin())
+	{
+		auto prev = FindRepr(i1, lindex, { std::prev(it)->first,it->first });
+		if (prev.first != -1 && l1.X[prev.first] > x1)
+			SetRepr(i1, lindex, { std::prev(it)->first,x1 }, Reprs);
+	}
+	auto itEnd = addV.lower_bound({ x2, sizeX() });
+	if (itEnd != addV.end())
+	{
+		auto prev = FindRepr(itEnd->second, lindex, { std::prev(itEnd)->first,itEnd->first });
+		if (prev.first != -1 && l1.X[prev.first] < x2)
+			SetRepr(itEnd->second, lindex, { x2,itEnd->first }, Reprs);
+	}
+	while (it != itEnd)
+		it = addV.erase(it); 
+	addV.emplace(x1, i1);
+	addV.emplace(x2, i);
+	SetRepr(i, lindex, { x1,x2 }, Reprs);
+	return;
+}
+
+pair<int, double> Segment::FindRepr(int i, int lindex, pair<double, double> intv)
+{
+	Segment& l1 = *Adjs[lindex].first;
+	intv = { max(intv.first,AdjDiagram[lindex][i].first), min(intv.second,AdjDiagram[lindex][i].second) };
+	auto it1 = l1.Sbar.lower_bound(intv.first);
+	auto it2 = l1.Sbar.upper_bound(intv.second);
+	if (intv.first > intv.second || it1->first > intv.second || it2 == l1.Sbar.begin())
+		return { -1,0. };
+	it2--;
+	int index;
+	auto D = [=](int n)->double {
+		return dist[i] + VecSize(l1.a + (l1.b - l1.a) * l1.X[n] - a - (b - a) * X[i]);
+	};
+	if (Near[lindex][i] <= it1->first)
+		index = it1->second;
+	else if (Near[lindex][i] >= it2->first)
+		index = it2->second;
+	else
+	{
+		it2 = l1.Sbar.lower_bound(Near[lindex][i]);
+		if (it2->first == Near[lindex][i])
+			index = it2->second;
+		else
+		{
+			it1 = it2;
+			it1--;
+			if (D(it1->second) > D(it2->second))
+				index = it2->second;
+			else
+				index = it1->second;
+		}
+	}
+	return { index,D(index) };
+}
+
+void Segment::SetRepr(int i, int lindex, pair<double, double> intv, priority_queue<Repr, vector<Repr>>& Reprs)
+{   
+	auto res = FindRepr(i, lindex, intv);
+	if (res.first == -1)
+		return;
+	Repr p;
+	p.src.index = i;
+	p.src.l = this;
+	p.dst.l = Adjs[lindex].first;
+	p.dst.index = res.first;
+	p.val = res.second;
+	Reprs.push(p);
+}
+
+void Segment::SetReprInv(int i, int lindex, priority_queue<Repr, vector<Repr>>& Reprs)
+{
+	Segment& l1 = *Adjs[lindex].first;
+	auto& addV = l1.AddVoronoi[Revs[lindex]];
+	auto it = addV.lower_bound({ X[i],-1 });
+	if (i == 0)
+		it = std::next(addV.begin());
+	if (it == addV.end())
+		it--;
+	pair<double, double> intv(std::prev(it)->first, it->first);
+	int index = it->second;
+	if (i == l1.FindRepr(index, Revs[lindex], intv).first)
+	{
+		Sbar.erase(X[i]);
+		l1.SetRepr(index, Revs[lindex], intv, Reprs);
+		Sbar[X[i]] = i;
+	}
+	return;
 }
 
 bool Segment::IsVvertex(int i, int i1, Segment& l1, double t)
--- dnn/Algorithms/apx_3DSP/3DSP/3DSP/propagation.h
+++ dnn/Algorithms/apx_3DSP/3DSP/3DSP/propagation.h
@@ -4,29 +4,35 @@
 #include <numeric>
 #include <limits>
 #include <algorithm>
+#include <queue>
+#include <map>
 #include "dots.h"
 
 using namespace std;
 
 struct PointOnSeg;
+struct Repr;
 
 class Segment {
 private:
+	bool tetra;
 	MyVec a,b;
 	vector<double> X;
 	vector<double> dist;
 	vector<PointOnSeg> prev;
-	set<int> S;
-	set<int> Sbar;
+	map<double, int> S;
+	map<double, int> Sbar;
 	vector<Tri> Tris; //Segment를 정의하는 두 face
 	vector<pair<Segment*, int>> Adjs;
+	vector<int> Revs; //Adjs[lindex].first.Adjs[Revs[lindex]].first == this, Adjs[lindex].first에서 Representative update하기 위해 필요
 	vector<vector<pair<double, double>>> AdjDiagram;
-	// k번째 원소 O(log n)에 계산가능한 tree 구현해서 수정해야 함
-	vector<set<pair<double,int>>> AddVoronoi; //set의 각 원소는 (vertex 상대위치, +방향 cell의 site)
+	// k번째 원소 O(log n)에 계산가능한 tree 구현해서 수정해야 함, 아니면 operator 수정해서 decision 문제 해결 가능하게 만들기(x*<x판별하는 decision은 변수 1개 dummy로 만들면 됨)
+	vector<set<pair<double,int>>> AddVoronoi; //set의 각 원소는 (vertex 상대위치, -방향 cell의 site)
 	vector<vector<double>> Near;
 	
 	//Steiner point generation 완성된 이후에 구현 예정
 	void SetAdjs() {};
+	void SetRevs() {};
 	void SetAdjDiagram();
 	void SetNear();
 
@@ -34,6 +40,10 @@ private:
 	pair<double, double> Interval(int i, MyVec& v1, MyVec& v2, MyVec& v3, Tri& f, Segment& l1);
 	bool IsVvertex(int i, int i1, Segment& l1, double t);
 	bool IsContain(int i, int i1, Segment& l1, double t);
+	void UpdateSeg(int i, int lindex, priority_queue<Repr, vector<Repr>>& Reprs);
+	pair<int, double> FindRepr(int i, int lindex, pair<double, double> intv);
+	void SetRepr(int i, int lindex, pair<double,double> intv, priority_queue<Repr, vector<Repr>>& Reprs);
+	void SetReprInv(int i, int lindex, priority_queue<Repr, vector<Repr>>& Reprs); //Sbar에서 vertex가 사라져 representative를 수정해야 하는 경우
 
 public:
 	Segment(MyVec _a, MyVec _b, vector<double> _X) {
@@ -42,17 +52,24 @@ public:
 		X = vector<double>(_X);
 		S.clear();
 		for (size_t i = 0; i < X.size(); i++)
-			Sbar.insert(i);
+		{
+			Sbar[X[i]]=i;
+		}
 		SetAdjs();
+		SetRevs();
 		SetAdjDiagram();
 		SetNear();
 	}
 	int sizeX() {
 		return X.size();
 	}
-	
+
+	bool IsTetra() {
+		return tetra;
+	}
+	   
 	bool IsActive(int i);
-	void UpdateV(int i, int lindex);
+	void Update(int i, double val, priority_queue<Repr, vector<Repr>>& Reprs);
 };
 
 
@@ -60,11 +77,9 @@ public:
 struct PointOnSeg {
 	Segment* l;
 	int index;
-	bool tetra;
 	PointOnSeg() {
 		l = nullptr;
 		index = -1;
-		tetra = false;
 	}
 };
 
