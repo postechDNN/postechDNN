fe9583e75733a03cee1a7000c2a3d9a86dac81e3
juny2400 <tmdwns1174@naver.com>
2021-09-29 13:52:21 +0900

    :arrow_up: 3D drawing example

---
+++ dnn/DataStructures/dDCEL/dUtil.h
@@ -0,0 +1,37 @@
+#ifndef DDCEL_UTIL
+#define DDCEL_UTIL
+
+#include <vector>
+#include <cassert>
+class dPoint;
+class dEdge;
+
+constexpr double ERR = 1e-6;
+
+template <typename BinaryOperation>
+dPoint* pts_fcn(dPoint* p1, dPoint* p2, BinaryOperation binary_op) {
+	assert(p1->getd() == p2->getd());
+	std::vector<double> *tmp = new std::vector<double>;
+	tmp->reserve(p1->getd());
+	transform(p1->cbegin(), p1->cend(), p2->cbegin(),
+		back_inserter(*tmp), binary_op);
+	return new dPoint(tmp);
+}
+
+dPoint* pts_sum(dPoint*, dPoint*);
+dPoint* pts_dif(dPoint*, dPoint*);
+bool same_vec(dPoint*, dPoint*);
+
+bool dCoplanar(dEdge*, dEdge*);
+bool dCoplanar(std::vector<dEdge*>);
+bool dCoplanar(dPoint*, dPoint*, dPoint*, dPoint*);
+bool dCoplanar(std::vector<dPoint*>);
+
+//require {std::min, std::max, <} operations
+template <typename T>
+bool is_between(T a, T b, T x) {
+	if (x < std::min(a, b) || std::max(a, b) < x) return false;
+	return true;
+}
+
+#endif
