2f8559542b72a7e9de2205d49c75ee3ecd82c450
Taehoon Ahn <sloth@postech.ac.kr>
2022-04-21 15:37:02 +0900

    :arrow_up: added Point codes

--- dnn/NearestNeighbor/EpsGraph3D/Point.h
+++ dnn/NearestNeighbor/EpsGraph3D/Point.h
@@ -1,3 +1,23 @@
 #pragma once
 
-class Point{};
+class Point{
+protected:
+	double x, y, z;
+
+public:
+
+	Point();
+	Point(double, double, double);
+	Point(Point*);
+	~Point();
+	bool operator==(Point);
+	Point operator- (Point);
+	double getx(void);
+	void setx(double);
+	double gety(void);
+	void sety(double);
+	double getz(void);
+	void setz(double);
+
+	double distance(Point);
+};
