161c0b7ef765e14e097aa1fd3359dda8e6a1b443
tkeom0114 <tkeom0114@postech.ac.kr>
2022-04-22 17:00:30 +0900

    :bug:Fix bugs in AVLTree

--- dnn/DataStructures/AVLTree/AVLTree.h
+++ dnn/DataStructures/AVLTree/AVLTree.h
@@ -8,6 +8,7 @@ struct AVLTreeNode {
 	AVLTreeNode();
 	~AVLTreeNode();
 	int rank; // stores a rank of the node
+	int size; // stores size of the subtree
 	T value; // stores a data
 	AVLTreeNode *l, *r, *p; // l stores left child, r stores right child, p stores parent. Initialized as nullptr
 };
@@ -16,6 +17,7 @@ template <typename T>
 class AVLTree {
 private:
 	AVLTreeNode<T>* root;
+	//need to update size
 	void LRotate(AVLTreeNode<T>*); // perform L rotate from subtree
 	void RRotate(AVLTreeNode<T>*); // perform R rotate from subtree
 	void balance(AVLTreeNode<T>*); // balance the subtree
@@ -23,6 +25,7 @@ private:
 public:
 	AVLTree();
 	~AVLTree();
+	int size();
 	bool isEmpty();
 	void insert(T);
 	void insert(AVLTreeNode<T>*);
@@ -31,6 +34,7 @@ public:
 	AVLTreeNode<T>* getRightNode(T);
 	AVLTreeNode<T>* pop();
 	AVLTreeNode<T>* pop(T);
+	AVLTreeNode<T>* getkthNode(int k);
 };
 
 template <typename T>
@@ -52,19 +56,23 @@ AVLTreeNode<T>::~AVLTreeNode() {
 template <typename T>
 void AVLTree<T>::LRotate(AVLTreeNode<T>* n) {
 	AVLTreeNode<T>* temp;
+	int tempsize = n->size;
 	if (n == this->root)
 		this->root = n->l;
 	else if (n == n->p->l)
 		n->p->l = n->l;
-	else
+	else  
 		n->p->r = n->l;
 	temp = n->p;
 	n->p = n->l;
 	n->l->p = temp;
 	temp = n->l->r;
+	int sz = temp ? temp->size : 0;
 	if (temp)
 		temp->p = n;
 	n->l->r = n;
+	n->size += sz - n->l->size;
+	n->l->size = tempsize;
 	n->l = temp;
 	calRank(n);
 	temp = n;
@@ -77,6 +85,7 @@ void AVLTree<T>::LRotate(AVLTreeNode<T>* n) {
 template <typename T>
 void AVLTree<T>::RRotate(AVLTreeNode<T>* n) {
 	AVLTreeNode<T>* temp;
+	int tempsize = n->size;
 	if (n == this->root)
 		this->root = n->r;
 	else if (n == n->p->l)
@@ -87,9 +96,12 @@ void AVLTree<T>::RRotate(AVLTreeNode<T>* n) {
 	n->p = n->r;
 	n->r->p = temp;
 	temp = n->r->l;
+	int sz = temp ? temp->size : 0;
 	if (temp)
 		temp->p = n;
 	n->r->l = n;
+	n->size += sz - n->r->size;
+	n->r->size = tempsize;
 	n->r = temp;
 	calRank(n);
 	temp = n;
@@ -144,9 +156,15 @@ bool AVLTree<T>::isEmpty() {
 }
 
 template <typename T>
+int AVLTree<T>::size() {
+	return this->root->size;
+}
+
+template <typename T>
 void AVLTree<T>::insert(T v) {
 	AVLTreeNode<T>* newnode = new AVLTreeNode<T>();
 	newnode->value = v;
+	newnode->size = 1;
 	this->insert(newnode);
 }
 
@@ -159,6 +177,7 @@ void AVLTree<T>::insert(AVLTreeNode<T>* newnode) {
 	}
 	AVLTreeNode<T>* temp = this->root;
 	while (true) {
+		temp->size++;
 		if (temp->value < newnode->value) {
 			if (!temp->r) {
 				temp->r = newnode;
@@ -242,8 +261,10 @@ AVLTreeNode<T>* AVLTree<T>::pop() {
 	if (!this->root)
 		return nullptr;
 	AVLTreeNode<T>* temp = this->root;
-	while (temp->l)
+	while (temp->l) {
+		temp->size--;
 		temp = temp->l;
+	}
 	if (temp == this->root) {
 		this->root = temp->r;
 		if (temp->r)
@@ -264,6 +285,7 @@ AVLTreeNode<T>* AVLTree<T>::pop() {
 	return temp;
 }
 
+//수정 필요함
 template <typename T>
 AVLTreeNode<T>* AVLTree<T>::pop(T v) {
 	if (!this->root)
@@ -271,18 +293,28 @@ AVLTreeNode<T>* AVLTree<T>::pop(T v) {
 	AVLTreeNode<T>* temp = this->root;
 	while (true) {
 		if (temp->value == v) {
+			temp->size--;
+			/*
 			if (temp == this->root) {
 				this->root = nullptr;
 				return temp;
-			}
+			}*/
 			if (!temp->l) {
+				if (temp == this->root) {
+					this->root = temp->r;
+					if (temp->r)
+						temp->r->p = nullptr;
+					return temp;
+				}
+				if (temp->r)
+					temp->r->p = temp->p;
 				if (temp->p->l == temp)
 					temp->p->l = temp->r;
 				else
 					temp->p->r = temp->r;
 				AVLTreeNode<T>* newtemp = temp->p;
 				calRank(newtemp);
-				balance(newtemp);
+				balance(newtemp);  
 				while (newtemp != this->root) {
 					newtemp = newtemp->p;
 					calRank(newtemp);
@@ -291,6 +323,12 @@ AVLTreeNode<T>* AVLTree<T>::pop(T v) {
 				return temp;
 			}
 			else if (!temp->r) {
+				if (temp == this->root) {
+					this->root = temp->l;
+					temp->l->p = nullptr;
+					return temp;
+				}
+				temp->l->p = temp->p;
 				if (temp->p->l == temp)
 					temp->p->l = temp->l;
 				else
@@ -307,9 +345,17 @@ AVLTreeNode<T>* AVLTree<T>::pop(T v) {
 			}
 			else {
 				AVLTreeNode<T>* stemp = temp->l;
-				while (stemp->r)
+				stemp->size--;
+				while (stemp->r) {
 					stemp = stemp->r;
-				stemp->p->r = stemp->l;
+					stemp->size--;
+				}
+				if (stemp->p != temp)
+					stemp->p->r = stemp->l;
+				else
+					stemp->p->l = stemp->l;
+				if (stemp->l)
+					stemp->l->p = stemp->p;
 				AVLTreeNode<T>* newtemp = stemp->p;
 				calRank(newtemp);
 				balance(newtemp);
@@ -319,19 +365,30 @@ AVLTreeNode<T>* AVLTree<T>::pop(T v) {
 					balance(newtemp);
 				}
 				stemp->p = temp->p;
-				if (temp->p->l == temp)
-					temp->p->l = stemp;
-				else
-					temp->p->r = stemp;
 				stemp->rank = temp->rank;
 				stemp->l = temp->l;
+				stemp->size = temp->size;
 				if (temp->l)
 					temp->l->p = stemp;
 				stemp->r = temp->r;
 				if (temp->r)
 					temp->r->p = stemp;
+				if (this->root == temp) {
+					this->root = stemp;
+					return temp;
+				}
+				if (temp->p->l == temp)
+					temp->p->l = stemp;
+				else
+					temp->p->r = stemp;
 				newtemp = stemp;
+				/*
 				while (newtemp != temp) {
+					newtemp = newtemp->p;
+					calRank(newtemp);
+					balance(newtemp);
+				}*/
+				while (newtemp != this->root) {
 					newtemp = newtemp->p;
 					calRank(newtemp);
 					balance(newtemp);
@@ -340,14 +397,52 @@ AVLTreeNode<T>* AVLTree<T>::pop(T v) {
 			}
 		}
 		else if (temp->value < v) {
-			if (!temp->r)
+			if (!temp->r) {
+				while (temp != this->root) {
+					temp = temp->p;
+					temp->size++;
+				}
 				return nullptr;
+			}
+			temp->size--;
 			temp = temp->r;
 		}
 		else {
-			if (!temp->l)
+			if (!temp->l) {
+				while (temp != this->root) {
+					temp = temp->p;
+					temp->size++;
+				}
 				return nullptr;
+			}
+			temp->size--;
 			temp = temp->l;
 		}
+	}
+}
+
+template<typename T>
+AVLTreeNode<T>* AVLTree<T>::getkthNode(int k)
+{
+	AVLTreeNode<T>* temp = root;
+	int psize = temp->size;
+	while (true)
+	{
+		int tsize = temp->size;
+		if (k >= tsize)
+			return nullptr;
+		if (temp->l && k == temp->l->size || !temp->l && k == 0)
+			return temp;
+		if (temp->l && k < temp->l->size)
+			temp = temp->l;
+		else if (temp->r)
+		{
+			k--;
+			if (temp->l)
+				k -= temp->l->size;
+			temp = temp->r;
+		}
+		else
+			return nullptr;
 	}
 }
