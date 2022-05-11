950e4a49e052ddc5f6d79974d6621aca7442f2e3
정재훈 <sk7755@postech.ac.kr>
2022-05-11 19:03:36 +0900

    :art: EdgeIntersection Algorithm

---
+++ dnn/Algorithms/EdgesIntersection/EdgesIntersection.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "AVLTree.h"
+#include <iostream>
+#include <string>
+#include "Edge.h"
+#include "Point.h"
+#include <queue>
+#include <set>
+#include <cmath>
+#include "Graph.h"
+#include <utility>
+
+#define tolerance 1e-6 //TODO: change to enum
+#define __DEBUG_MODE__ 0
+
+class EdgeContainer{
+public:
+    int key;
+    std::vector<Edge> *edges;
+    Point *sweep_p;
+    EdgeContainer();
+    EdgeContainer(int k,std::vector<Edge>* edges, Point *sweep_p);
+    EdgeContainer(const EdgeContainer& op);
+    double getKey() const;
+    bool operator <(const EdgeContainer& op) const;
+    bool operator==(const EdgeContainer& op) const;
+};
+
+class intersectEvent{
+public:
+    enum class EVENT{START =0 , END, CROSS};
+    Point p;
+    int key;
+    EVENT ev;
+    intersectEvent(const Point& p, int key ,EVENT ev);
+    bool operator<(const intersectEvent op) const;
+    bool operator==(intersectEvent& op);
+};
+
+class EdgesIntersection{ //output 고민좀 해보자
+private: 
+    Point sweep_p;
+    AVLTree<EdgeContainer> T;
+    std::priority_queue<intersectEvent> pq; 
+    std::vector<Edge> edges;
+    Graph<Point> graph;
+    void FindNewEvent(EdgeContainer ec_1, EdgeContainer ec_2);
+public: 
+    EdgesIntersection(std::vector<Edge> edges);
+    Graph<Point> getGraph();
+};
+
