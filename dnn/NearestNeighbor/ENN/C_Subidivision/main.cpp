bedd7364d52d47334e1094a043be8cd328b5c7a4
정재훈 <sk7755@naver.com>
2023-07-05 23:13:43 +0900

    :art: add additional checking

--- dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
+++ dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
@@ -8,7 +8,7 @@
 #include "CS_valid_check.h"
 
 int main(){
-    std::vector<Point> pts = {Point(0.5,0.5),Point(4.5,4.5)};
+    std::vector<Point> pts = {Point(0.5,0.5),Point(4.5,4.5),Point(400.5,400.5)}; //
     C_Subdivision csubdiv(pts);
 
     std::cout<<"scale_factor: "<<csubdiv.scale_factor<<std::endl;
@@ -20,31 +20,37 @@ int main(){
     }
     int alpha = 1;
 
-    DCEL dcel = csubdiv.build_d_subdivision(alpha);
+    DCEL dcel = csubdiv.build_alpha_subdivision(alpha);
     //DCEL_IO::print_DCEL(dcel);
-    std::string he_key = "e2_1";
+    // std::string he_key = "e2_1";
 
-    HEdge *he = dcel.getHedge(he_key);
-    if(!he){
-        std::cout <<"There is no key of half edges in DCEL"<<std::endl;
-        return 0;
-    }
-    WC_region wc(dcel, he);
+    // HEdge *he = dcel.getHedge(he_key);
+    // if(!he){
+    //     std::cout <<"There is no key of half edges in DCEL"<<std::endl;
+    //     return 0;
+    // }
+    // WC_region wc(dcel, he);
     
-    std::cout << "Well-covering region of "<<he_key<<": ";
-    for(auto f:wc.regions){
-        std::cout<<f->getKey()<<' ';
-    }
-    std::cout<<std::endl;
-    std::cout << "Boundary of this well-covering region is: ";
-    for(auto he:wc.boundary){
-        std::cout<<he->getKey()<<' ';
-    }
-    std::cout<<std::endl;
-    std::cout <<"W1: "<<check_w1(dcel)<<std::endl;
-    std::cout <<"W2: "<<check_w2(dcel,alpha)<<std::endl;
-    //std::cout <<"W3: "<<check_w3(dcel,alpha)<<std::endl;
-    std::cout<<"A1: "<<check_a1(dcel)<<std::endl;
-    std::cout<<"A4: "<<check_a4(dcel,alpha)<<std::endl;
+    // std::cout << "Well-covering region of "<<he_key<<": ";
+    // for(auto f:wc.regions){
+    //     std::cout<<f->getKey()<<' ';
+    // }
+    // std::cout<<std::endl;
+    // std::cout << "Boundary of this well-covering region is: ";
+    // for(auto he:wc.boundary){
+    //     std::cout<<he->getKey()<<' ';
+    // }
+    // std::cout<<std::endl;
+
+    std::cout <<"Strong alpha-conforming subdivision space test\n";
+    if(check_strong_csdiv(pts,dcel,alpha))
+        std::cout<<"pass all tests\n";
+    else std::cout <<"fail some test\n";
+
+    std::cout <<"\nAdditional properties of alpha-conforming subdivision test\n";
+    if(check_add_properties(pts,dcel,alpha))
+        std::cout<<"pass all tests\n";
+    else std::cout <<"fail some test\n";
+
     return 0;
 }
