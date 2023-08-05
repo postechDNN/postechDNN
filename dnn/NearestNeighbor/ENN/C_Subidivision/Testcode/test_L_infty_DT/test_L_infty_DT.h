8c6d8f4e8f7797effc08e895d4c2dbe442d2e947
정재훈 <sk7755@naver.com>
2023-08-05 21:54:31 +0900

    :art: write a test code for L infinity delaunay triangulation.

--- dnn/NearestNeighbor/ENN/C_Subidivision/Testcode/test_L_infty_DT/test_L_infty_DT.h
+++ dnn/NearestNeighbor/ENN/C_Subidivision/Testcode/test_L_infty_DT/test_L_infty_DT.h
@@ -2,8 +2,13 @@
 #include "../../DCEL/Point.h"
 #include "../../DCEL/Edge.h"
 #include "../../DCEL/Vector.h"
+#include <limits>
 #include <vector>
 
 //Graph is an adjacency list of Delaunay triangulation which is an undirected graph whose vertices are stored in sites.
 //Return true if it is a L-infty Delaunay triangulation, otherwise return false.
-bool test_L_infty_DT(std::vector<Point> sites, std::vector< std::vector<int>> graph);
+bool test_L_infty_DT(std::vector<Point>&, std::vector< std::vector<int>> &);
+bool test_L_infty_DT_condition(DCEL &);
+bool test_triangulation(std::vector<Point>& , std::vector< std::vector<int>> &);
+
+
--- dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
+++ dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
@@ -3,8 +3,15 @@
 #include <algorithm>
 #include <vector>
 #include "Testcode/test_csdiv_pts/test_csdiv_pts.h"
+#include "Testcode/test_L_infty_DT/test_L_infty_DT.h"
 
 int main(){
-    test_csdiv_pts();
+    std::vector<Point> pts = {Point(0,0), Point(4,1), Point(2,2)};
+    std::vector<std::vector<int>> graph(3);
+    graph[0].push_back(1); graph[0].push_back(2);
+    graph[1].push_back(0); graph[1].push_back(2);
+    graph[2].push_back(1); graph[2].push_back(0);
+    if(test_L_infty_DT(pts,graph))
+        std::cout <<"pass L-infinity Delaunay triangulation test!"<<std::endl;
     return 0;
 }
