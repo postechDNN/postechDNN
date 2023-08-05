39d5d045fd0fd91ef6a63856181d011757c4fea2
kbu0417 <kbu417@postech.ac.kr>
2023-08-05 22:31:42 +0900

    Implemented construct DCEL function for obstacles and classify vertices type and half edge type function.

--- dnn/NearestNeighbor/ENN/C_Subidivision/CS_Free.cpp
+++ dnn/NearestNeighbor/ENN/C_Subidivision/CS_Free.cpp
@@ -1,5 +1,7 @@
 #include "CS_Free.h"
 
+DCEL constructObsDCEL(std::vector<SimplePolygon>& obstacles, std::string key = "__default__");
+
 CS_Free::CS_Free(Point src, std::vector<SimplePolygon>& obstacles){
     //1. Collect all points of obstacles and source.
     std::vector<Point> pts={src};
@@ -13,13 +15,39 @@ CS_Free::CS_Free(Point src, std::vector<SimplePolygon>& obstacles){
     DCEL S = CS.build_alpha_subdivision(2);
 
     //3. Construct DCEL for obstacles and label vertices and half edges of the DCEL.
-    DCEL D_obs;
+    DCEL D_obs = constructObsDCEL(obstacles);
     
     //4. Merge the conforming subdivision and the DCEL for obstacles into S_overlay.
     DCEL S_overlay = S.merge(D_obs);
 
     //5. Store vertices type and half edge types by using labels of S_overlay. 
-
+    {
+        std::vector<HEdge*> edges = S_overlay.getHedges();
+        std::vector<Vertex*> vertices = S_overlay.getVertices();
+        for (auto e : edges) {
+            std::string key = e->getKey();
+            if (key.front() == "_") {
+                this->edge_types[key] = TRP;
+            }
+            else {
+                this->edge_types[key] = OPQ;
+            }
+        }
+        for (auto v : vertices) {
+            std::string key = v->getKey();
+            Point pos = v->getPoint();
+            //src
+            if (std::abs(src.getx() - pos.getx()) < 1e-6 && std::abs(src.gety() - pos.gety()) < 1e-6) {
+                this->vertices_types[key] = SRC;
+            }
+            else if (key.front() == "_") {
+                this->vertices_types[key] = OBS;
+            }
+            else {
+                this->vertices_types[key] = TRP;
+            }
+        }
+    }
     //6. Keep intact the cells in S_overaly which is incident to an obstacle vertex or S, 
     //  and delete every edge fragment of S not on the boundary of one of these interesting cells.
     // HOW: Uninteresting cell 구하고, interesting cell의 half edge만 key를 모아두고
@@ -31,3 +59,28 @@ CS_Free::CS_Free(Point src, std::vector<SimplePolygon>& obstacles){
 }
 
 CS_Free::~CS_Free(){}
+
+DCEL constructObsDCEL(std::vector<SimplePolygon>& obstacles, std::string key) {
+    //std::vector<Point>&, std::vector<std::vector<int>>&
+    std::vector<Point> vertex;
+    std::vector<std::vector<int>> graph;
+
+    for (auto nowPolygon : obstacles) {
+        int startIdx = vertex.size();
+        std::vector<Point> nowPts = nowPolygon.getVertices();
+        for (auto p : nowPts) point.push_back(p);
+        for (int i = 0; i < nowPts.size() - 1; i++) {
+            std::vector<int> edge;
+            edge.push_back(startIdx + i);
+            edge.push_back(startIdx + i + 1);
+            graph.push_back(edge);
+        }
+        std::vector<int> edge;
+        edge.push_back(startIdx + nowPts.size() - 1);
+        edge.push_back(startIdx);
+        graph.push_back(edge);
+    }
+
+    DCEL ret(point, graph, key);
+    return ret;
+}
