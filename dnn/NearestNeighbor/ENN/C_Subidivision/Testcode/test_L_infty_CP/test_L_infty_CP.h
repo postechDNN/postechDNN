d0156f697e80065bbbe7f0f567b154e6b789feb5
정재훈 <sk7755@naver.com>
2023-08-16 20:37:42 +0900

    :art: test code for L infinity closest pair

--- dnn/NearestNeighbor/ENN/C_Subidivision/Testcode/test_L_infty_CP/test_L_infty_CP.h
+++ dnn/NearestNeighbor/ENN/C_Subidivision/Testcode/test_L_infty_CP/test_L_infty_CP.h
@@ -3,5 +3,5 @@
 #include "../../DCEL/Point.h"
 #include <limits>
 
-bool test_L_infty_CP(std::vector<Point> &, int ,int);
+bool test_L_infty_CP(std::vector<Point> &, double);
 double inline L_infty_dist(Point&, Point&);
