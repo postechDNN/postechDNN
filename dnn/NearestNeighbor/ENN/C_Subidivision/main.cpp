cebabd8bfdf4646980f7d8064ee5c057d7437e2e
kbu0417 <kbu417@postech.ac.kr>
2023-11-27 15:44:11 +0900

    :memo: test code for CS Free construction implemented.

--- dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
+++ dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
@@ -3,8 +3,16 @@
 #include <algorithm>
 #include <vector>
 #include "Testcode/test_csdiv_pts/test_csdiv_pts.h"
+#include "CS_Free.h"
 
 int main(){
     test_csdiv_pts();
+
+    // To test CS_Free construction
+    Point src;
+    std::vector<SimplePolygon> obstacles;
+    
+    //CS_Free cs(src, obstacles);
+
     return 0;
 }
