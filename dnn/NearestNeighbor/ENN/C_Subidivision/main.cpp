20271e8ab7bf6f10bd6dfdd40183f625844c189a
정재훈 <sk7755@naver.com>
2023-12-04 15:03:08 +0900

    :art: test main file

--- dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
+++ dnn/NearestNeighbor/ENN/C_Subidivision/main.cpp
@@ -6,13 +6,15 @@
 #include "CS_Free.h"
 
 int main(){
-    test_csdiv_pts();
+    //test_csdiv_pts();
 
     // To test CS_Free construction
-    Point src;
-    std::vector<SimplePolygon> obstacles;
-    
-    //CS_Free cs(src, obstacles);
-
+    Point src(0,0);
+    std::vector<Point> pts1 = {Point(4,0), Point(0,2), Point(2,6)};
+    std::vector<Point> pts2 = {Point(5,1), Point(5.5,5),Point(8.5,5), Point(8.5,3)};
+    std::vector<SimplePolygon> obstacles={SimplePolygon(pts1), SimplePolygon(pts2)};
+    std::cout << "cs free construction start";
+    CS_Free cs(src, obstacles);
+    std::cout << "cs free construction done";
     return 0;
 }
