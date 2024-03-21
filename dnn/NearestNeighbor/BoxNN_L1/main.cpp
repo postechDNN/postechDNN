edaafd88d921993a08c0d377ce2b68e8feb152e0
jagunlee <jagunlee@postech.ac.kr>
2024-03-22 00:52:19 +0900

    add pt

--- dnn/NearestNeighbor/BoxNN_L1/main.cpp
+++ dnn/NearestNeighbor/BoxNN_L1/main.cpp
@@ -1,1 +1,7 @@
+#include <list>
+#include <vector>
 
+int main() {
+	std::cout << "Hello World";
+	return 0;
+}
---
+++ dnn/NearestNeighbor/BoxNN_L1/point.h
@@ -0,0 +1,15 @@
+#include <vector>
+
+class Point {
+public:
+	int n;
+	std::vector<double> xs;
+	Point();
+	Point(int);
+	Point(std::vector<double>);
+	Point(Point*);
+	~Point();
+	bool operator==(Point);
+	double getx(int);
+	void setxs(std::vector<double>);
+};
