f189018a30499534677b4298d8b905a57cfcec52
Chaeyoon Chung <chaeyoon17@postech.ac.kr>
2024-04-23 18:07:29 +0900

    :art: Comment unused Delaunay part to let Makefile work

--- dnn/NearestNeighbor/ENN/C_Subidivision/Graph.h
+++ dnn/NearestNeighbor/ENN/C_Subidivision/Graph.h
@@ -132,33 +132,33 @@ int Graph::kruskalMST()
 } 
 
 // Driver program to test above functions 
-int main() 
-{ 
-	/* Let us create above shown weighted 
-	and undirected graph */
-	int V = 9, E = 14; 
-	Graph g(V, E); 
+// int main() 
+// { 
+// 	/* Let us create above shown weighted 
+// 	and undirected graph */
+// 	int V = 9, E = 14; 
+// 	Graph g(V, E); 
 
-	// making above shown graph 
-	g.addEdge(0, 1, 4); 
-	g.addEdge(0, 7, 8); 
-	g.addEdge(1, 2, 8); 
-	g.addEdge(1, 7, 11); 
-	g.addEdge(2, 3, 7); 
-	g.addEdge(2, 8, 2); 
-	g.addEdge(2, 5, 4); 
-	g.addEdge(3, 4, 9); 
-	g.addEdge(3, 5, 14); 
-	g.addEdge(4, 5, 10); 
-	g.addEdge(5, 6, 2); 
-	g.addEdge(6, 7, 1); 
-	g.addEdge(6, 8, 6); 
-	g.addEdge(7, 8, 7); 
+// 	// making above shown graph 
+// 	g.addEdge(0, 1, 4); 
+// 	g.addEdge(0, 7, 8); 
+// 	g.addEdge(1, 2, 8); 
+// 	g.addEdge(1, 7, 11); 
+// 	g.addEdge(2, 3, 7); 
+// 	g.addEdge(2, 8, 2); 
+// 	g.addEdge(2, 5, 4); 
+// 	g.addEdge(3, 4, 9); 
+// 	g.addEdge(3, 5, 14); 
+// 	g.addEdge(4, 5, 10); 
+// 	g.addEdge(5, 6, 2); 
+// 	g.addEdge(6, 7, 1); 
+// 	g.addEdge(6, 8, 6); 
+// 	g.addEdge(7, 8, 7); 
 
-	cout << "Edges of MST are \n"; 
-	int mst_wt = g.kruskalMST(); 
+// 	cout << "Edges of MST are \n"; 
+// 	int mst_wt = g.kruskalMST(); 
 
-	cout << "\nWeight of MST is " << mst_wt; 
+// 	cout << "\nWeight of MST is " << mst_wt; 
 
-	return 0; 
-} 
+// 	return 0; 
+// } 
