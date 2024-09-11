4cacdd9d71b3571fdd6a70b3e453c0edb0fde943
Chaeyoon Chung <chaeyoon17@postech.ac.kr>
2024-09-11 15:13:57 +0900

    :art: compute_apx_wf

--- dnn/NearestNeighbor/ENN/C_Subidivision/APX_wavefront.h
+++ dnn/NearestNeighbor/ENN/C_Subidivision/APX_wavefront.h
@@ -19,4 +19,19 @@ public:
     ~APX_wavefront();
 
     std::vector<Point> get_generators();
+    std::pair<Point, Point> compute_claim_range(HEdge* e, int idx); 
+    std::pair<Point, Point> compute_claim_range(HEdge* e, Point & pt); 
 };
+
+
+std::vector<Point> APX_wavefront::get_generators(){
+    
+}
+
+std::pair<Point, Point> APX_wavefront::compute_claim_range(HEdge* e, int idx){
+    //
+}
+
+std::pair<Point, Point> APX_wavefront::compute_claim_range(HEdge* e, Point & pt){
+
+}
