8434380bbb3c91652fffb2492cc31bbb4a05ec93
kbu0417 <kbu417@postech.ac.kr>
2024-09-20 18:25:33 +0900

    :art: Made Hyperbola files.

--- dnn/NearestNeighbor/ENN/C_Subidivision/SPM.h
+++ dnn/NearestNeighbor/ENN/C_Subidivision/SPM.h
@@ -3,50 +3,9 @@
 #include <vector>
 #include <string>
 #include <map>
+#include "Hyperbola.h"
 #include "WF_propagation.h"
 //#include "DCEL/DCEL.h"
-
-class Hyperbola {
-private:
-	Point s, t; // endpoint
-	Point translation;
-	double rotation;
-	double a, b; // x^2/a^2 - y^2/b^2 = 1
-public:
-	Hyperbola() {}
-	Hyperbola(Point s, Point t, Point translation, double rotation, double a, double b) 
-		: s(s), t(t), translation(translation), rotation(rotation), a(a), b(b) {}
-	Hyperbola(Hyperbola* HB) {
-		this->s = HB->gets();
-		this->t = HB->gett();
-		this->translation = HB->getTranslation();
-		this->rotation = HB->getRotation();
-		this->a = HB->geta();
-		this->b = HB->getb();
-	}
-	~Hyperbola() {}
-
-	void sets(Point s) { this->s = s; }
-	void sett(Point t) { this->t = t; }
-	void setTranslation(Point translation) { this->translation = translation; }
-	void setRotation(double rotation) { this->rotation = rotation; }
-	void seta(double a) { this->a = a; }
-	void setb(double b) { this->b = b; }
-
-	Point gets() const { return this->s; }
-	Point gett() const { return this->t; }
-	Point getTranslation() const { return this->translation; }
-	double getRotation() const { return this->rotation; }
-	double geta() const { return this->a; }
-	double getb() const { return this->b; }
-
-	void flip() {
-		Point temp = this->s;
-		this->s = this->t;
-		this->t = temp;
-	}
-};
-
 
 class SFaces {
 private:
